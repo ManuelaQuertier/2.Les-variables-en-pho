2f82faff4aff49224262114d9c3d57d407eeb179
Manuela Quertier <manuelaquertier@gmail.com>
2021-09-24 21:34:05 +0200

    add firts code for my quest

---
+++ lesVariablesenPHP/Indy.php
@@ -0,0 +1,11 @@
+<?php
+$movie = "Indiana Jones and the Last Crusade"; #string
+$seen = TRUE; #boolean
+$year = 1989; #integer
+$rating = 8.2; #float
+
+echo $movie;
+echo $seen;
+echo $year;
+echo $rating;
+?>
